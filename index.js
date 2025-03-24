7e9bb21a912f74ff87e4553aeaed1e32829407d8
Hiruna Dilmith <hirunadilmith12@gmail.com>
2025-03-25 01:35:16 +0530

    Can retrieve character details now

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 
   const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+  const query_channel = await client.channels.fetch(process.env.QUERY_CHANNEL);
 
   let oldestMessage = null;
   let lastMessage = null;
@@ -14,6 +15,42 @@ client.on('ready', async () => {
   let rolls = [];
   let claims = [];
   let gives = [];
+
+  let roll_commands = process.env.ROLL_COMMANDS.split(',');
+
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  //function to query the character details
+  async function query_character(name){    
+    const filter = m => m.author.username.includes('Mudae') && m.embeds.length>0;    
+    while (true){
+      try{        
+        await query_channel.send('$im '+name);
+        const collected = await query_channel.awaitMessages({filter, max: 1, time: 10000, errors: ['time']});
+        const embed = collected.first().embeds[0];        
+        let char_name = embed.author.name;
+        let series = null;        
+
+        if (embed.description.includes('female')){
+          series = embed.description.match(/^(.*?)<:female/)[1].trim();
+        } else if (embed.description.includes('male')){
+          series = embed.description.match(/^(.*?)<:male/)[1].trim();
+        } 
+
+        let kakera = embed.description.match(/\*  · \*\*(.*?)<:kakera/)[1].trim();
+        let claim_rank = embed.description.match(/Claim Rank: #(.*?)\n/)[1].trim();
+        let like_rank = embed.description.match(/Like Rank: #([\d,]+)/)[1].trim();              
+                
+        console.log(`\nName: ${char_name}\nSeries: ${series}\nKakera: ${kakera}\nClaim Rank: ${claim_rank}\nLike Rank: ${like_rank}\n`);
+        break;
+      } catch (error){
+        console.log(' ERROR while querying :  '+error);        
+      }
+      
+    }
+  }
 
   try{
     const messages = await channel.messages.fetch({limit:10, lastMessage});
@@ -24,7 +61,10 @@ client.on('ready', async () => {
         timestamp: null
     };    
 
-    await messages.forEach(async (msg) => {
+    // console.log('Size : '+ messages.size);
+    // console.log('Last message: ' + messages.last().createdAt)
+
+    for (const [index,msg] of messages.entries()) {      
       //normal roll
         if (msg.author.username == 'Mudae' && msg.embeds.length>0 && msg.embeds[0].description.includes('React with any emoji to claim') && msg.interaction == null){
             const roll = msg.embeds[0];
@@ -33,7 +73,7 @@ client.on('ready', async () => {
 
             obj.name = name;
             obj.series = series;
-            obj.timestamp = moment(msg.createdAt).format("YYYY-MM-DD HH:mm:ss");
+            obj.timestamp = moment(msg.createdAt).format("YYYY-MM-DD HH:mm:ss");            
 
         //slash command roll
         } else if (msg.author.username == 'Mudae' && msg.embeds.length>0 && msg.embeds[0].description.includes('React with any emoji to claim') && msg.interaction != null) {
@@ -47,7 +87,8 @@ client.on('ready', async () => {
           obj.owner = msg.interaction.user.username;          
 
           if (!Object.values(obj).includes(null)){            
-            rolls.push({...obj});            
+            rolls.push({...obj});        
+            Object.assign(obj, {name: null, series: null, timestamp: null});    
           }  
 
         //out of rolls        
@@ -56,12 +97,14 @@ client.on('ready', async () => {
           Object.assign(obj, {name: null, series: null, timestamp: null});
 
         //roll message
-        } else if (msg.content == '$m'){
+        } else if (roll_commands.some(comm => msg.content == comm)){
             obj.owner = msg.author.username;
 
             if (!Object.values(obj).includes(null)){              
-              rolls.push({...obj});              
-            }      
+              rolls.push({...obj});                        
+              await query_character(obj.name);
+              Object.assign(obj, {name: null, series: null, timestamp: null});    
+            }                  
         //claims
         } else if (msg.author.username == 'Mudae' && msg.content.includes('are now married')){
           const split_msg = msg.content.split(' and ');
@@ -70,15 +113,16 @@ client.on('ready', async () => {
           const timestamp = moment(msg.createdAt).format("YYYY-MM-DD HH:mm:ss");
 
           claims.push({name: char, owner: owner, timestamp: timestamp});
-          // console.log(claims);
+          
         
         //giving characters
         } else if (msg.author.username == 'Mudae' && msg.content.includes('given to')){
           const split_msg = msg.content.split(' given to ');
-          const chars = split_msg[0].match(/\*\*(.*?)\*\*/)[1];
+          const chars = split_msg[0];
           const user_id = split_msg[1].match(/<@(\d+)>/)[1];
           
           const owner = await client.users.fetch(user_id);
+          const timestamp = moment(msg.createdAt).format("YYYY-MM-DD HH:mm:ss");
           
           let character;
 
@@ -89,12 +133,12 @@ client.on('ready', async () => {
           }
 
           character.forEach(c => {
-            gives.push({name: c, owner: owner.username});            
+            gives.push({name: c.match(/\*\*(.*?)\*\*/)[1], owner: owner.username, timestamp: timestamp});            
           })
-        }
-    });    
+        }        
+    };    
     
-    console.log(gives);
+    // console.log(`Rolls : ${rolls.length}\nClaims : ${claims.length}\nGives : ${gives.length}`);
         
   } catch (error){
     console.log(error);
